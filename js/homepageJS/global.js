986e2fe6cff7bbd778d38ad0fa2be6bb07e3b0a0
unknown <harshada@lexstart.in>
2018-11-20 18:19:28 +0530

    path changed

--- js/homepageJS/global.js
+++ js/homepageJS/global.js
@@ -1,5 +1,5 @@
 // $(document).ready(function(){
 //   if(window.location.pathname== "/getstarted"){
-//     window.location.pathname = "getstarted/getting_started.html"
+//     window.location.pathname = "getting_started.html"
 //   }
 // })
