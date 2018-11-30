a5dbecd5325f3f3f0dfab3b63b30cb9967b7a100
unknown <harshada@lexstart.in>
2018-11-30 14:27:37 +0530

    form changes

--- js/homepageJS/global.js
+++ js/homepageJS/global.js
@@ -8,7 +8,7 @@ $(window).scroll(function(){
 
 $('.dropdown').hover(function() {
 	$(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
-	$('.dropdown-menu').css('top','55px');
+	// $('.dropdown-menu').css('top','55px');
   }, function() {
 	$(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
   });
